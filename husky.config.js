752fa859a762308365e5a0e507b99e733c0fdbae
Magno Biét <magno.biet@gmail.com>
2020-07-14 18:41:06 -0300

    chore: add React Testing Library integration

--- husky.config.js
+++ husky.config.js
@@ -1,12 +1,9 @@
 /* eslint-env es6, node */
 
-function tasks(commands) {
-  return commands.join(' && ');
-}
-
 module.exports = {
   hooks: {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    'pre-commit': tasks(['lint-staged']),
+    'pre-commit': 'lint-staged',
+    'pre-push': 'npm run test -- --findRelatedTests --bail',
   },
 };
