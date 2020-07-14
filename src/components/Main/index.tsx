2e0f645218c774ed466ea29309077a3f89333194
Magno Biét <magno.biet@gmail.com>
2020-07-14 19:35:50 -0300

    chore: add Main page and test

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,16 @@
+import * as S from './styles';
+
 const Main = () => (
-  <div>
-    <h1>React</h1>
-  </div>
+  <S.Wrapper>
+    <S.Logo src="/images/logo.svg" alt="" />
+
+    <S.Title>Advanced React Course</S.Title>
+
+    <S.Description>
+      TypeScript, ReactJS, NextJS and Styled Components
+    </S.Description>
+    <S.Illustration src="/images/hero-illustration.svg" alt="" />
+  </S.Wrapper>
 );
 
 export default Main;
