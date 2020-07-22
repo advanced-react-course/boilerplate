a2f1ee8c057fb21e103b7f3c2a19bcfb9f202f9b
Magno Biét <magno.biet@gmail.com>
2020-07-22 20:51:04 -0300

    chore: ignore stories on code coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.js(x)'],
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
