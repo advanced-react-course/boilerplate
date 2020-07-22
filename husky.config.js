ad0faea1f957e106af9f277dd9e0d1c3ef2236b0
Magno Biét <magno.biet@gmail.com>
2020-07-22 20:48:22 -0300

    refactor: remove Git pre-push hook

--- husky.config.js
+++ husky.config.js
@@ -4,6 +4,5 @@ module.exports = {
   hooks: {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     'pre-commit': 'lint-staged',
-    'pre-push': 'npm run test -- --findRelatedTests --bail',
   },
 };
