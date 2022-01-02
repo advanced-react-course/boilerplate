f94f5f532aff86154d1a14658013de1c298b2df3
Magno Fernando Brixner Biét <magno.biet@gmail.com>
2022-01-02 09:14:35 -0300

    refactor: update Plop templates

--- .plop/plopfile.js
+++ .plop/plopfile.js
@@ -1,36 +1,62 @@
 module.exports = (plop) => {
-
   plop.setGenerator('component', {
     description: 'Create a component',
     prompts: [
       {
         type: 'input',
         name: 'name',
-        message: 'What is your component name?'
-      }
+        message: 'What is your component name?',
+      },
     ],
     actions: [
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/index.tsx',
-        templateFile: './templates/component/index.tsx.hbs'
+        templateFile: './templates/component/index.tsx.hbs',
       },
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/styles.ts',
-        templateFile: './templates/component/styles.ts.hbs'
+        templateFile: './templates/component/styles.ts.hbs',
       },
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/stories.tsx',
-        templateFile: './templates/component/stories.tsx.hbs'
+        templateFile: './templates/component/stories.tsx.hbs',
       },
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/test.tsx',
-        templateFile: './templates/component/test.tsx.hbs'
-      }
-    ]
+        templateFile: './templates/component/test.tsx.hbs',
+      },
+    ],
   });
 
+  plop.setGenerator('template', {
+    description: 'Create a page template',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your template name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/index.tsx',
+        templateFile: './templates/template/index.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/styles.ts',
+        templateFile: './templates/component/styles.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/test.tsx',
+        templateFile: './templates/template/test.tsx.hbs',
+      },
+    ],
+  });
 };
